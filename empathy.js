f3024fbdfeec6959a162bc7a293c3cb02537cc37
Alan Gutierrez <alan@prettyrobots.com>
2016-06-01 12:19:20 -0500

    Going to put this in Compassion for now.

--- empathy.js
+++ empathy.js
@@ -1,11 +1,11 @@
 var cadence = require('cadence')
 
-function Empathy () {
+function Confer () {
     this._particpants = []
     this._transitioning = {}
 }
 
-Empathy.prototype._message = cadence(function (async, timeout, message) {
+Confer.prototype._message = cadence(function (async, timeout, message) {
     if (message.government) {
         if (message.collapsed) {
             this._broadcasts.clear()
@@ -46,7 +46,18 @@ Empathy.prototype._message = cadence(function (async, timeout, message) {
         // The split between naturalization and immigration doesn't mean we wait
         // for a participant to naturalize before it can be used within the
         // application, only that it must naturalize before it can become a
-        // member of the government.
+        // member of the government. This eliminates any concerns that our
+        // leader would assume leadership without the application state. This
+        // addition of eventual naturalization bought a lot and simplified
+        // reasonsing about initalization.
+
+        // Can even sort out back channel synchronization, but we need to still
+        // take advantage of the ability to replay. Leader connects directly to
+        // immigrant, marks the version of the database in the log, streams
+        // state then tells the log to resume for the participant. There can be
+        // a maximum log length held by participant. Pause is a message that
+        // causes the participants kibitzer to pause the log, scanning it for
+        // when to resume the log.
 
         // We could track the exiles and then remove the tracking when they
         // become real according to Misnomer. But, this doesn't work because
@@ -63,17 +74,17 @@ Empathy.prototype._message = cadence(function (async, timeout, message) {
     }
 })
 
-Empathy.prototype.message = function (message) {
+Confer.prototype.message = function (message) {
     if (message.government || message.namespace == 'bigeasy.empathy') {
         this._messages.push(message)
     }
 }
 
-Empathy.prototype.register = function (registration) {
+Confer.prototype.register = function (registration) {
     this._registrations[registration.name] = registration
 }
 
-Empathy.prototype.initiate = function () {
+Confer.prototype.initiate = function () {
 }
 
-module.exports = Empathy
+module.exports = Confer
